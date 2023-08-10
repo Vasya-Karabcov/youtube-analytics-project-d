2667a24b6bca69aaabc4b29593eeafbe9209799d
Василий Карабцов <karabcovas@mail.ru>
2023-08-10 23:21:50 +0300

    готовый ютуб

--- src/channel.py
+++ src/channel.py
@@ -10,7 +10,7 @@ class Channel:
     """Класс для ютуб-канала"""
 
     @classmethod
-    def get_service():
+    def get_service(cls):
         api_key: str = os.getenv('API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
 
@@ -18,38 +18,18 @@ class Channel:
     def channel_id(self):
         return self.__channel_id
 
-    @channel_id.setter
-    def channel_id(self, channel_id):
-        if channel_id == 'Новое название':
-            raise AttributeError(self.__channel_id)
-
-
-
-    @property
-    def channel(self):
-        youtube = Channel.g
-        return youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-        channel = self.channel
+
+        channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
         self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
-
-    @classmethod
-    def get_service(self):
-        api_key: str = os.getenv('API_KEY')
-        return build('youtube', 'v3', developerKey=api_key)
-
-    @property
-    def channel(self):
-        youtube = Channel.get_service()
-        return youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     # def print_info(self) -> None:
     #     """Выводит в консоль информацию о канале."""
@@ -62,5 +42,14 @@ class Channel:
     #
     #     print(json.dumps(channel, indent=2, ensure_ascii=False))
 
-    def to_json(self):
-        pass
+    def to_json(self, json_file):
+        info_channel = {'channel_id': self.__channel_id,
+                        'title': self.title,
+                        'description': self.description,
+                        'url': self.url,
+                        'subscriber_count': self.subscriber_count,
+                        'video_count': self.video_count,
+                        'view_count': self.view_count,
+                        }
+        with open(json_file, 'w') as outfile:
+            json.dump(info_channel, outfile, indent=2, ensure_ascii=False,)
