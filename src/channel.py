b6f9ae61e5276e082f4930359a7d15955de3d554
Василий Карабцов <karabcovas@mail.ru>
2023-08-13 17:25:31 +0300

    готовый ютуб

--- src/channel.py
+++ src/channel.py
@@ -31,6 +31,27 @@ class Channel:
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other): #больше
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __lt__(self, other): #меньше
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __ge__(self, other): # Больше или равно
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __le__(self, other): #Меньше или равно
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
     # def print_info(self) -> None:
     #     """Выводит в консоль информацию о канале."""
     #     api_key: str = os.getenv('API_KEY')
